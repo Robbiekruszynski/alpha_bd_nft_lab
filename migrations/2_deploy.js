766b5b9c4e60f5c6167ae31fb53d706146827738
Robbie Kruszynski <robbiekruszynski@Robbies-MacBook-Pro-2.local>
2021-12-14 19:05:31 +0100

    update deploy

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,5 +3,5 @@
 const ERC721PresetMinterPauserAutoId = artifacts.require("ERC721PresetMinterPauserAutoId");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC721PresetMinterPauserAutoId, "Blockdaemon Protocol Ambassador","BPA", "https://my-json-server.typicode.com/abcoathup/samplenft/tokens/");
+  deployer.deploy(ERC721PresetMinterPauserAutoId, "Blockdaemon Protocol Ambassador","BPA", "https://my-json-server.typicode.com/robbiekruszynski/alpha_bd_nft_db/tokens/");
 };
